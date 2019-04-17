4dedd98e94433c33e779214ffbfe7f01a02d6829
Seth Mcbay <sethmcbay@yahoo.com>
2019-04-17 14:28:05 -0400

    added app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import ArtistList from "./components/SpotList";
+import Artist from "./components/Spot";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+    render() {
+        return (
+            <Router>
+                <div className="App">
 
+                    <div>
+                        <h1>Surferosa</h1>
+                        <div>
+                            <div><Link to="/">All Spots</Link></div>
+                        </div>
+                    </div>
+
+                    <Switch>
+                      <Route exact path="/" component={SpotList}/>
+                      <Route path="/Spot/:id" component={Spot}/>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
 export default App;
